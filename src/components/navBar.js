982a45c397534a6c3f92d41bed1010606495519b
Daniel Fadamitan <104998213+Invalid8@users.noreply.github.com>
2023-08-08 13:19:18 +0000

    new pushes

---
+++ src/components/navBar.js
@@ -0,0 +1,21 @@
+const NavBar = () => {
+    return ( 
+        <div className="navHeader">
+            <div className="logo">
+                YummyTaste
+            </div>
+            <div className="navLinks">
+                <div className="showContentOnSmallerSreen">
+                    <button className="menuBtn">MENU</button>
+                </div>
+                <div className="options">
+                    <ul>
+                        li
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default NavBar;
