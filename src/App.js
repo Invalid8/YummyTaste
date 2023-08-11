2ef33ce6d4011a81f18769c3fbaacf53f66baf82
Daniel Fadamitan <104998213+Invalid8@users.noreply.github.com>
2023-08-11 19:04:10 +0000

    go go go

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './Pages/About';
 import Blog from './Pages/Blog';
 import Contact from './Pages/Contact';
 import NavBar from './components/NavBar';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Analytics />
     </>
   );
 }
