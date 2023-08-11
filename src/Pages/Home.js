1d48449d9e693b35a51b654408c1620898417a8d
Daniel Fadamitan <104998213+Invalid8@users.noreply.github.com>
2023-08-11 18:41:48 +0000

    ahhhh mad edit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,21 @@
+import { Link } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import PlaceholderImage from "../images/mini.jpg";
+
 const Home = () => {
     return ( 
-        <div className="container">
+        <div className="container" style={{gap: '10px'}}>
             <h1>Welcome to Yummytaste</h1>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum vero sit labore veniam qui nam quibusdam iure ea debitis nostrum!</p>
-            <img src="https://cdn.tasteatlas.com//images/toplistarticles/08c818739e4b48ce96d319c16f4cc0ca.jpg?mw=1300" alt="..." style={{maxWidth: '840px', width: '100%', margin: '0 auto'}}/>
+            <button className="nBtn"><Link to="/Menu">Get a Meal</Link></button>
+            <div className="imgBox">
+                <LazyLoadImage 
+                    src="https://cdn.tasteatlas.com//images/toplistarticles/08c818739e4b48ce96d319c16f4cc0ca.jpg?mw=1300" 
+                    alt="..."
+                    placeholderSrc={PlaceholderImage}
+                    effect="blur"
+                />
+            </div>
         </div>
     );
 }
